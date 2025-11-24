c61a1e2aac16730cf928ab419acf3da5c42b5128
ale <tomescualexandramirela@gmail.com>
2025-11-24 12:22:59 +0200

    feat: add top scores pop-up

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 // open tutorial pop-up when clicking on tutorial button
 const tutorialBtn = document.getElementById('tutorial');
 const tutorialOverlay = document.getElementById('tutorial-overlay');
-const closeBtn = document.getElementById('close-icon');
+const closeTutorialBtn = document.getElementById('close-tutorial-icon');
 
 tutorialBtn.addEventListener('click', () => {
     tutorialOverlay.style.display = 'flex';
 });
 
-closeBtn.addEventListener('click', () => {
+closeTutorialBtn.addEventListener('click', () => {
     tutorialOverlay.style.display = 'none';
+});
+
+// open highscores pop-up when clicking on trophy icon
+const hsBtn = document.getElementById('trophy-icon');
+const hsOverlay = document.getElementById('hs-overlay');
+const closeHsBtn = document.getElementById('close-hs-icon');
+
+hsBtn.addEventListener('click', () => {
+    hsOverlay.style.display = 'flex';
+});
+
+closeHsBtn.addEventListener('click', () => {
+    hsOverlay.style.display = 'none';
 });
